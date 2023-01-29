d239c451993eb3f6d60e64978dfb8d803302a917
sheetalj2205 <sheetalj2205@gmail.com>
2023-01-29 11:05:49 +0530

    Feat: Repository added

--- src/main/java/com/tala/poc/demo/repository/UserRepository.java
+++ src/main/java/com/tala/poc/demo/repository/UserRepository.java
@@ -1,4 +1,11 @@
 package com.tala.poc.demo.repository;
 
-public class UserRepository {
+import com.tala.poc.demo.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
 }
+
