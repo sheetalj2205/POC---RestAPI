f9ec77fa9a176156b396eb05323cc96bce56fa7c
sheetalj2205 <sheetalj2205@gmail.com>
2023-01-29 11:33:40 +0530

    Feat: Exceptions file added

--- src/main/java/com/tala/poc/demo/exception/ErrorDetails.java
+++ src/main/java/com/tala/poc/demo/exception/ErrorDetails.java
@@ -1,2 +1,28 @@
-package com.tala.poc.demo.exception;public class ErrorDetails {
+package com.tala.poc.demo.exception;
+
+import java.util.Date;
+
+public class ErrorDetails {
+    private Date timestamp;
+    private String message;
+    private String details;
+
+    public ErrorDetails(Date timestamp, String message, String details) {
+        super();
+        this.timestamp = timestamp;
+        this.message = message;
+        this.details = details;
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getDetails() {
+        return details;
+    }
 }
